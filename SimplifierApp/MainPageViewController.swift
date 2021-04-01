c727db3b53f0ba9701ab06bf036d300a48fbbc86
Олеся Мартынюк <olesyamartinyuk@yandex.ru>
2021-04-01 14:55:08 +0300

    Added API request call

--- SimplifierApp/MainPageViewController.swift
+++ SimplifierApp/MainPageViewController.swift
@@ -7,19 +7,42 @@
 
 import UIKit
 
+
 class MainPageViewController: UIViewController {
 
-    @IBOutlet weak var defaultTextField: UITextField!
-    @IBOutlet weak var simplifiedTextField: UITextField!
+   
+    @IBOutlet weak var defaultTextField: UITextView!
+    @IBOutlet weak var simplifiedTextField: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: true)
     }
     
     @IBAction func simplifyButtonPressed(_ sender: Any) {
+        let url = URL(string: "https://test-simplifier.herokuapp.com/simplify")
+        guard let requestUrl = url else { fatalError() }
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        let postString = "text=\(defaultTextField.text!)";
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Error took place \(error)")
+                    return
+                }
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("Response data string:\n\(dataString)")
+                    DispatchQueue.main.async {
+                        self.simplifiedTextField.text = dataString
+                    }
+                }
+        }
+        task.resume()
     }
+    
     @IBAction func bookmarksButtonPressed(_ sender: Any) {
     }
+    
     @IBAction func settingsButtonPressed(_ sender: Any) {
     }
 }
